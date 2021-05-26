9607e807c01d36b16c782a0a2cb712dcaeab7a3a
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-05-27 01:14:27 +0500

    navbar components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { AppBar, Button, Toolbar, Grid } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+import { LOGIN_ROUTE } from "../util/const";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const user = false;
+  return (
+    <AppBar
+      style={{ background: "linear-gradient(-45deg, #39249a, #e14e42)" }}
+      position="static"
+    >
+      <Toolbar variant="dense">
+        <h3>Samar Badriddinov</h3>
+        <Grid container justify={"flex-end"}>
+          {user ? (
+            <Button variant={"outlined"}>Quit</Button>
+          ) : (
+            <NavLink to={LOGIN_ROUTE}>
+              <Button variant={"outlined"}>Login</Button>
+            </NavLink>
+          )}
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
