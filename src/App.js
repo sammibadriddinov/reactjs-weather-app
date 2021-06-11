93da3aed1d88f7a03e98aa896b3cf3c2d7a958f4
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-06-11 16:19:41 +0500

    bismillah

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { useContext } from "react";
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./components/AppRouter";
-import Navbar from "./components/Navbar";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Context } from ".";
-import Loader from "./components/Loader";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const { auth } = useContext(Context);
-  const [user, loading, error] = useAuthState(auth);
-  if (loading) {
-    return <Loader />;
-  }
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRouter />
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/util/const.js
+++
@@ -1,2 +0,0 @@
-export const LOGIN_ROUTE = "/login";
-export const CHAT_ROUTE = "/chat";
